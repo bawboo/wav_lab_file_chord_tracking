f206f456efa96847736b9c7ca12b539c9925e7c3
bawboo <42883691+bawboo@users.noreply.github.com>
2024-05-24 16:32:09 +0800

    Update app.js

--- app.js
+++ app.js
@@ -213,3 +213,219 @@ function playPause() {
     playBtn.html('PLAY');
     waveState = waveStates.PAUSE;
   } else if (waveState == waveStates.PAUSE) {
+    wavesurfer.play();
+    wavesurfer2.play();
+    wavesurfer3.play();
+    playBtn.html('PAUSE');
+    waveState = waveStates.PLAY;
+  }
+}
+
+function readChordFile(file, waveObj) {
+  let base64data = file.data.replace('data:application/octet-stream;base64,','');
+
+  let chordText = atob(base64data);
+  let chordLabels = chordText.split('\n');
+  visualizeChords(waveObj, chordLabels);
+}
+
+function readMainChordFile(file) {
+  readChordFile(file, wavesurfer);
+}
+
+function readOtherChordFile(file) {
+  otherChordFile = file;
+  initLoading();
+  wavesurfer2.load(waveData);
+}
+
+function readThirdChordFile(file) {
+  thirdChordFile = file;
+  initLoading();
+  wavesurfer3.load(waveData);
+}
+/******************************/
+
+/******************************/
+/* Music visualization        */
+function initWaveforms() {
+  wavesurfer = WaveSurfer.create({
+    container: '#waveform',
+    waveColor: 'violet',
+    progressColor: 'purple',
+    scrollParent: true,
+    minPxPerSec: 80,
+    plugins: [
+      WaveSurfer.cursor.create({
+        showTime: true,
+        opacity: 0.5,
+        customShowTimeStyle: {
+          'padding': '5px',
+          'padding-top': '100px',
+          'font-size': '12px'
+        }
+      }),
+      WaveSurfer.regions.create({}),
+      WaveSurfer.markers.create({}),
+      WaveSurfer.timeline.create({
+        container: "#wave-timeline",
+        timeInterval: 1,
+      })
+    ]
+  });
+
+  wavesurfer.on('ready', function () {
+    finishLoading();
+  });
+
+  wavesurfer.on('finish', function () {
+    playBtn.html('PLAY');
+    waveState = waveStates.PAUSE;
+  });
+
+  wavesurfer2 = WaveSurfer.create({
+    container: '#waveform2',
+    waveColor: 'violet',
+    progressColor: 'purple',
+    scrollParent: true,
+    minPxPerSec: 80,
+    interact: false,
+    hideScrollbar: true,
+    plugins: [
+      WaveSurfer.regions.create({}),
+      WaveSurfer.markers.create({}),
+      WaveSurfer.timeline.create({
+        container: "#wave-timeline2",
+        timeInterval: 1,
+      })
+    ]
+  });
+
+  wavesurfer2.setMute(true);
+  wavesurfer.on('seek', (progress) => {
+    wavesurfer2.seekAndCenter(progress);
+  });
+
+  wavesurfer2.on('ready', function () {
+    readChordFile(otherChordFile, wavesurfer2);
+    finishLoading();
+
+    let progress = wavesurfer.getCurrentTime() / wavesurfer.getDuration();
+    wavesurfer2.seekAndCenter(progress);
+    if (wavesurfer.isPlaying())
+      wavesurfer2.play();
+  });
+
+  wavesurfer2.on('finish', function () {
+    playBtn.html('PLAY');
+    waveState = waveStates.PAUSE;
+  });
+
+  // Initialize wavesurfer3
+  wavesurfer3 = WaveSurfer.create({
+    container: '#waveform3',
+    waveColor: 'violet',
+    progressColor: 'purple',
+    scrollParent: true,
+    minPxPerSec: 80,
+    interact: false,
+    hideScrollbar: true,
+    plugins: [
+      WaveSurfer.regions.create({}),
+      WaveSurfer.markers.create({}),
+      WaveSurfer.timeline.create({
+        container: "#wave-timeline3",
+        timeInterval: 1,
+      })
+    ]
+  });
+
+  wavesurfer3.setMute(true);
+  wavesurfer.on('seek', (progress) => {
+    wavesurfer3.seekAndCenter(progress);
+  });
+
+  wavesurfer3.on('ready', function () {
+    readChordFile(thirdChordFile, wavesurfer3);
+    finishLoading();
+
+    let progress = wavesurfer.getCurrentTime() / wavesurfer.getDuration();
+    wavesurfer3.seekAndCenter(progress);
+    if (wavesurfer.isPlaying())
+      wavesurfer3.play();
+  });
+
+  wavesurfer3.on('finish', function () {
+    playBtn.html('PLAY');
+    waveState = waveStates.PAUSE;
+  });
+}
+
+function visualizeChords(waveObj, chordLabels) {
+  let baseColorMap = ["#4A9CBB", "#9D6AFF"];
+  let prevChord = 'N';
+
+  waveObj.clearRegions();
+  waveObj.clearMarkers();
+
+  chordLabels.forEach((line, i) => {
+    if (!line) // empty
+      return; // continue
+
+    let labelComps = line.split('\t');
+    if (labelComps.length != 3)
+      return; // continue
+
+    let st = labelComps[0];
+    let ed = labelComps[1];
+    let chordName = labelComps[2];
+    chordName = chordName.replace(':maj7','M7').replace(':min7','m7')
+                         .replace(':maj','').replace(':min', 'm').replace(':','');
+
+    if ((chordName == "N") || (chordName == "X"))
+      return; // continue
+
+    let color = "#4A9CBB"; // default
+    if (chordName in chordColor)
+      color = chordColor[chordName];
+
+    if (chordName != prevChord) {
+      waveObj.addMarker({
+        time: st,
+        label: chordName,
+        color: color.concat("EE"),
+        position: "top"
+      });
+    }
+
+    waveObj.addRegion({
+      start: st,
+      end: ed,
+      color: color.concat("77"),
+      drag: false, resize: false
+    });
+
+    prevChord = chordName;
+  });
+}
+/******************************/
+
+/******************************/
+/* ML processing              */
+
+// TODO
+async function initAutoChordModel() {
+  try {
+    console.log('Loading chord recognition model...');
+    //window.model = await tf.loadLayersModel('autochord-model/model.json');
+    // window.model = await tf.loadGraphModel('models/tfjs/chroma-seq-bilstm-crf-0-base/model.json');
+    const modelUrl =
+    'https://storage.googleapis.com/tfjs-models/savedmodel/mobilenet_v2_1.0_224/model.json';
+    const model = await tf.loadGraphModel(modelUrl);
+
+    console.log('Loaded.');
+  } catch (e) {
+    console.log(e);
+  }
+}
+/******************************/
